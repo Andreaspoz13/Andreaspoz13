e78f008abf69f0eb7fd252a4dd335abbd4c00b5b
Andreaspoz13 <99254946+Andreaspoz13@users.noreply.github.com>
2022-02-08 13:09:26 +0200

    A REALLY NICE STORY MADE BY ANDREASPOZ13
    INSTRUCTIONS -->
    To Open the Program Install Visual Studio Code .NET SDK And Runtime.
    And After that create a folder and put those things in.Then open Visual studio Code and open the folder you created.
    Now click on the "Program.cs" file in Visual Studio Code AND FINALLY Go to the toolbar in the bottom of the window, Select Run--> Start Debugging.
    From now you should get the program on your Screen
    Also make sure to install the official C# Extension From Visual Studio Code's Extension Store before Running the Program 
                                          NOW ENJOY THE STORY!!

---
+++ A REALLY NICE STORY! Made by Andreaspoz13/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+              // Customization
+            Console.Title = "My First Project Made by Pozotos";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 99;
+
+            //Credits
+             Console.WriteLine("Created By Pozotos!\nPlease press ENTER key.You will need to do that every time you write something\n e.x:If you write your name and press enter then press ENTER again\n(do that only if the next line doesn't appear!)");
+            Console.ReadKey();
+            
+
+           // The Start of the Story
+             Console.WriteLine("Hello there. What's your name?");
+             
+             Console.ReadLine();
+             
+             Console.ReadKey();
+             Console.WriteLine("OK. Let's Get Started!\nStory writen by Pozotos");
+             Console.ReadKey();
+            Console.WriteLine("Once upon a time there was somebody named");
+             Console.ReadLine();
+             Console.WriteLine("...He/She Was Riding his bike while,Suddenly (something appeared)");
+             Console.ReadLine();
+             Console.WriteLine("...Appeared! But instead of him/her/it Doing Something.");
+             Console.WriteLine("*Write What he/she/it did*");
+             Console.ReadLine();
+            Console.WriteLine("Which was something that the biker for some reason liked!");
+            Console.WriteLine("And at that point he started...");
+            Console.ReadLine();
+            Console.WriteLine("AND WELL THIS IS THE END OF THIS REALLY NICE AND SHORT STORY");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+           Console.ReadKey();
+            Console.WriteLine("THE END!\n \n Credits:\n Andreas Pozotos\nMade with Visual Studio Code");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ReadKey();
+            Console.WriteLine("THANKS FOR DOWNLOADING!\n Press ENTER to EXIT");
+            Console.ReadKey();
+        
+        }   
+
+
+
+    }
+}
